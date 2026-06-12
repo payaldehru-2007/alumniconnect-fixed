bc49392460e38726e9114bbb95ea341166982d74
payaldehru-2007 <payaldehru@gmail.com>
2026-06-12 08:33:56 +0530

    Fix CORS for all methods

--- alumni-backend/api/alumni/[id].js
+++ alumni-backend/api/alumni/[id].js
@@ -7,7 +7,7 @@ const supabase = createClient(
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
